ae3f378588cfaf2a5d69214b0e9c7709ee5a58bd
LevelUp Developer <developer@levelup.com>
2026-04-21 15:37:06 +0545

    Remove Fade, Anivia, Annie and add Cookie Run: Kingdom

--- src/lib/other-games.ts
+++ src/lib/other-games.ts
@@ -355,10 +355,6 @@ export const otherGames = {
       "image": "https://media.valorant-api.com/agents/e370fa57-4757-3604-3648-499e1f642d3f/displayicon.png"
     },
     {
-      "name": "Fade",
-      "image": "https://media.valorant-api.com/agents/dade69b4-4f5a-8528-247b-219e5a1facd6/displayicon.png"
-    },
-    {
       "name": "Breach",
       "image": "https://media.valorant-api.com/agents/5f8d3a7f-467b-97f3-062c-13acf203c006/displayicon.png"
     },
@@ -491,14 +487,6 @@ export const otherGames = {
     {
       "name": "Amumu",
       "image": "https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/Amumu.png"
-    },
-    {
-      "name": "Anivia",
-      "image": "https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/Anivia.png"
-    },
-    {
-      "name": "Annie",
-      "image": "https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/Annie.png"
     },
     {
       "name": "Aphelios",
@@ -1847,6 +1835,116 @@ export const otherGames = {
     {
       "name": "Largo",
       "image": "https://api.opendota.com/apps/dota2/images/dota_react/heroes/icons/largo.png?"
+    }
+  ],
+  "Cookie Run: Kingdom": [
+    {
+      "name": "GingerBrave",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/0/05/GingerBrave_square.png"
+    },
+    {
+      "name": "Strawberry Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/a/ab/Strawberry_Cookie_square.png"
+    },
+    {
+      "name": "Wizard Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/9/91/Wizard_Cookie_square.png"
+    },
+    {
+      "name": "Chili Pepper Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/1/14/Chili_Pepper_Cookie_square.png"
+    },
+    {
+      "name": "Custard Cookie III",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/b/b3/Custard_Cookie_III_square.png"
+    },
+    {
+      "name": "Pure Vanilla Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/0/09/Pure_Vanilla_Cookie_square.png"
+    },
+    {
+      "name": "Hollyberry Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/f/f6/Hollyberry_Cookie_square.png"
+    },
+    {
+      "name": "Dark Cacao Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/a/a9/Dark_Cacao_Cookie_square.png"
+    },
+    {
+      "name": "Golden Cheese Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/4/4e/Golden_Cheese_Cookie_square.png"
+    },
+    {
+      "name": "White Lily Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/8/87/White_Lily_Cookie_square.png"
+    },
+    {
+      "name": "Espresso Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/f/f5/Espresso_Cookie_square.png"
+    },
+    {
+      "name": "Madeleine Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/7/77/Madeleine_Cookie_square.png"
+    },
+    {
+      "name": "Latte Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/1/1d/Latte_Cookie_square.png"
+    },
+    {
+      "name": "Vampire Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/0/0e/Vampire_Cookie_square.png"
+    },
+    {
+      "name": "Herb Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/e/ea/Herb_Cookie_square.png"
+    },
+    {
+      "name": "Dark Choco Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/d/df/Dark_Choco_Cookie_square.png"
+    },
+    {
+      "name": "Sea Fairy Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/6/69/Sea_Fairy_Cookie_square.png"
+    },
+    {
+      "name": "Frost Queen Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/2/2f/Frost_Queen_Cookie_square.png"
+    },
+    {
+      "name": "Black Pearl Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/4/44/Black_Pearl_Cookie_square.png"
+    },
+    {
+      "name": "Moonlight Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/6/62/Moonlight_Cookie_square.png"
+    },
+    {
+      "name": "Red Velvet Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/2/2e/Red_Velvet_Cookie_square.png"
+    },
+    {
+      "name": "Cotton Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/5/53/Cotton_Cookie_square.png"
+    },
+    {
+      "name": "Caramel Arrow Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/d/dd/Caramel_Arrow_Cookie_square.png"
+    },
+    {
+      "name": "Financier Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/2/22/Financier_Cookie_square.png"
+    },
+    {
+      "name": "Sorbet Shark Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/1/1c/Sorbet_Shark_Cookie_square.png"
+    },
+    {
+      "name": "Eclair Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/5/5f/Eclair_Cookie_square.png"
+    },
+    {
+      "name": "Wildberry Cookie",
+      "image": "https://static.wikia.nocookie.net/cookierunkingdom/images/5/58/Wildberry_Cookie_square.png"
     }
   ]
 };
--- updateFetch.js
+++ updateFetch.js
@@ -1,70 +1,18 @@
 const fs = require('fs');
-const https = require('https');
 
-async function fetchJson(url) {
-  return new Promise((resolve, reject) => {
-    https.get(url, (res) => {
-      let body = '';
-      res.on('data', chunk => body += chunk);
-      res.on('end', () => resolve(JSON.parse(body)));
-    }).on('error', reject);
-  });
-}
+let code = fs.readFileSync('fetchImages.js', 'utf8');
 
-async function main() {
-  const games = {};
+code = code.replace(
+  `games['Valorant'] = valResponse.data.map(a => ({`,
+  `games['Valorant'] = valResponse.data.filter(a => a.displayName.toLowerCase() !== 'fade').map(a => ({`
+);
 
-  // Genshin Impact
-  try {
-    const genshinResponse = await fetchJson('https://genshin.jmp.blue/characters');
-    const genshinChars = [];
-    for (const c of genshinResponse) {
-      if (c.includes('traveler')) continue;
-      genshinChars.push({
-        name: c.charAt(0).toUpperCase() + c.slice(1).replace('-', ' '),
-        image: `https://genshin.jmp.blue/characters/${c}/icon`
-      });
-    }
-    games['Genshin Impact'] = genshinChars;
-  } catch(e) { console.error('Genshin error', e); }
+code = code.replace(
+  `games['League of Legends'] = Object.values(lolResponse.data).map(c => ({`,
+  `games['League of Legends'] = Object.values(lolResponse.data).filter(c => !['anivia', 'annie'].includes(c.name.toLowerCase())).map(c => ({`
+);
 
-  // Valorant
-  try {
-    const valResponse = await fetchJson('https://valorant-api.com/v1/agents?isPlayableCharacter=true');
-    games['Valorant'] = valResponse.data.filter(a => a.displayName.toLowerCase() !== 'fade').map(a => ({
-      name: a.displayName,
-      image: a.displayIconSmall
-    }));
-  } catch(e) { console.error('Valorant error', e); }
-
-  // League of Legends
-  try {
-    const lolResponse = await fetchJson('https://ddragon.leagueoflegends.com/cdn/13.24.1/data/en_US/champion.json');
-    games['League of Legends'] = Object.values(lolResponse.data).filter(c => !['anivia', 'annie'].includes(c.name.toLowerCase())).map(c => ({
-      name: c.name,
-      image: `https://ddragon.leagueoflegends.com/cdn/13.24.1/img/champion/${c.image.full}`
-    }));
-  } catch(e) { console.error('LoL error', e); }
-
-  // Overwatch
-  try {
-    const owResponse = await fetchJson('https://overfast-api.tekrop.fr/heroes');
-    games['Overwatch'] = owResponse.map(h => ({
-      name: h.name,
-      image: h.portrait
-    }));
-  } catch(e) { console.error('OW error', e); }
-
-  // Dota 2
-  try {
-    const dotaResponse = await fetchJson('https://api.opendota.com/api/heroStats');
-    games['Dota 2'] = dotaResponse.map(h => ({
-      name: h.localized_name,
-      image: `https://api.opendota.com${h.icon}`
-    }));
-  } catch(e) { console.error('Dota error', e); }
-
-
+const crkSnippet = `
   // Cookie Run: Kingdom
   try {
     const cookies = [
@@ -99,10 +47,12 @@ async function main() {
     games['Cookie Run: Kingdom'] = cookies;
   } catch(e) { console.error('CRK error', e); }
 
-  let out = `export const otherGames = ${JSON.stringify(games, null, 2)};\n`;
+  let out = \`export const otherGames = \${JSON.stringify(games, null, 2)};\\n\`;
+`;
 
-  fs.writeFileSync('src/lib/other-games.ts', out);
-  console.log('Finished updating other-games.ts');
-}
+code = code.replace(
+  "  let out = `export const otherGames = ${JSON.stringify(games, null, 2)};\\n`;",
+  crkSnippet
+);
 
-main();
+fs.writeFileSync('fetchImages.js', code);
